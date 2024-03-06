f6d95770a2c471bdbb2fe6e8a0a291c9e6f6740a
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-06 10:59:23 +0100

    Refactor code for improved performance and readability

--- database/migrations/2024_03_06_095909_create_productperallergeen_table.php
+++ database/migrations/2024_03_06_095909_create_productperallergeen_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product', function (Blueprint $table) {
-            $table->id(); 
-            $table->string('Naam'); 
-            $table->string('Barcode'); 
+        Schema::create('productperallergeen', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('productperallergeen');
     }
 };
