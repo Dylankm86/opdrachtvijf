700cf734695f43299740da544efe9beb8581f537
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-06 11:04:46 +0100

    Add 'Naam' and 'Omschrijving' columns to 'allergeen' table

--- database/migrations/2024_03_06_100236_create_allergeen_table.php
+++ database/migrations/2024_03_06_100236_create_allergeen_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('allergeen', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
+            $table->string('Naam'); 
+            $table->text('Omschrijving'); 
             $table->timestamps();
         });
     }
