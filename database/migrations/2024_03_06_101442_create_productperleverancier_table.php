3a5871b2ddc7547fe12926d8dd00b787dcec42a3
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-06 11:16:34 +0100

    Update function to handle edge cases

---
+++ database/migrations/2024_03_06_101442_create_productperleverancier_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productperleverancier', function (Blueprint $table) {
+            $table->id(); 
+            $table->unsignedBigInteger('leverancier_id'); 
+            $table->foreign('leverancier_id')->references('id')->on('leveranciers'); 
+            $table->unsignedBigInteger('product_id'); +
+            $table->foreign('product_id')->references('id')->on('products'); 
+            $table->date('DatumLevering'); 
+            $table->integer('Aantal'); 
+            $table->date('DatumEerstVolgendeLevering')->nullable();     
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productperleverancier');
+    }
+};
