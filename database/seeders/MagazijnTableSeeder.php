387f4c317e87c446dfb4f5968d285e56877eaefe
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-06 11:36:08 +0100

    made product id nullabale and seeded the database

--- database/seeders/MagazijnTableSeeder.php
+++ database/seeders/MagazijnTableSeeder.php
@@ -12,21 +12,21 @@ class MagazijnTableSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        DB::table('magazijn')->insert([
-            ['id' => 1, 'ProductId' => 1, 'VerpakkingsEenheid' => 5, 'AantalAanwezig' => 453],
-            ['id' => 2, 'ProductId' => 2, 'VerpakkingsEenheid' => 2.5, 'AantalAanwezig' => 400],
-            ['id' => 3, 'ProductId' => 3, 'VerpakkingsEenheid' => 5, 'AantalAanwezig' => 1],
-            ['id' => 4, 'ProductId' => 4, 'VerpakkingsEenheid' => 1, 'AantalAanwezig' => 800],
-            ['id' => 5, 'ProductId' => 5, 'VerpakkingsEenheid' => 3, 'AantalAanwezig' => 234],
-            ['id' => 6, 'ProductId' => 6, 'VerpakkingsEenheid' => 2, 'AantalAanwezig' => 345],
-            ['id' => 7, 'ProductId' => 7, 'VerpakkingsEenheid' => 1, 'AantalAanwezig' => 795],
-            ['id' => 8, 'ProductId' => 8, 'VerpakkingsEenheid' => 10, 'AantalAanwezig' => 233],
-            ['id' => 9, 'ProductId' => 9, 'VerpakkingsEenheid' => 2.5, 'AantalAanwezig' => 123],
-            ['id' => 10, 'ProductId' => 10, 'VerpakkingsEenheid' => 3, 'AantalAanwezig' => null],
-            ['id' => 11, 'ProductId' => 11, 'VerpakkingsEenheid' => 2, 'AantalAanwezig' => 367],
-            ['id' => 12, 'ProductId' => 12, 'VerpakkingsEenheid' => 1, 'AantalAanwezig' => 467],
-            ['id' => 13, 'ProductId' => 13, 'VerpakkingsEenheid' => 5, 'AantalAanwezig' => 20],
-        ]);
-    }
+{
+    DB::table('magazijn')->insert([
+        ['VerpakkingsEenheid' => 5, 'AantalAanwezig' => 453],
+        ['VerpakkingsEenheid' => 2.5, 'AantalAanwezig' => 400],
+        ['VerpakkingsEenheid' => 5, 'AantalAanwezig' => 1],
+        ['VerpakkingsEenheid' => 1, 'AantalAanwezig' => 800],
+        ['VerpakkingsEenheid' => 3, 'AantalAanwezig' => 234],
+        ['VerpakkingsEenheid' => 2, 'AantalAanwezig' => 345],
+        ['VerpakkingsEenheid' => 1, 'AantalAanwezig' => 795],
+        ['VerpakkingsEenheid' => 10, 'AantalAanwezig' => 233],
+        ['VerpakkingsEenheid' => 2.5, 'AantalAanwezig' => 123],
+        ['VerpakkingsEenheid' => 3, 'AantalAanwezig' => null],
+        ['VerpakkingsEenheid' => 2, 'AantalAanwezig' => 367],
+        ['VerpakkingsEenheid' => 1, 'AantalAanwezig' => 467],
+        ['VerpakkingsEenheid' => 5, 'AantalAanwezig' => 20],
+    ]);
+}
 }
