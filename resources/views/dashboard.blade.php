0974803a1a38ae5d4e3928d7aebf9dab8bd2abc4
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-07 01:45:08 +0100

    simplefying the data retrieval to see if that works

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,28 +16,8 @@
     </div>
 
     <div class="container">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Barcode</th>
-                    <th>Name</th>
-                    <th>VerpakkingsEenheid</th>
-                    <th>AantalAanwezig</th>
-                    <th>Allergeen</th>
-                    <th>Leverancier</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($products as $product)
-                <tr>
-                    <td>{{ $product->Barcode }}</td>
-                    <td>{{ $product->Naam }}</td>
-                    <td>{{ $product->VerpakkingsEenheid }}</td>
-                    <td>{{ $product->AantalAanwezig }}</td>
-                    <td>{{ $product->LeverancierNaam }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        @foreach ($products as $product)
+        <p>{{ $product->Naam }}</p>
+        @endforeach
     </div>
 </x-app-layout>
