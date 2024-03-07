d9c181dacbb5688d22383dc95088597f0a30be89
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-07 02:03:40 +0100

    Add table structure and display product details in dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,27 @@
 @section('content')
 <h1>Dashboard</h1>
 
-@foreach ($products as $product)
-<p>{{ $product->Naam }}</p>
-@endforeach
+<table>
+    <thead>
+        <tr>
+            <th>Barcode</th>
+            <th>Naam</th>
+            <th>VerpakkingsEenheid</th>
+            <th>AantalAanwezig</th>
+            <th>LeverancierNaam</th>
+            <!-- Add more headers for the additional fields -->
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ $product->Barcode }}</td>
+            <td>{{ $product->Naam }}</td>
+            <td>{{ $product->VerpakkingsEenheid }}</td>
+            <td>{{ $product->AantalAanwezig }}</td>
+            <td>{{ $product->LeverancierNaam }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
