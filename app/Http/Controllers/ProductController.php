79d447200756eb721ea89c4c2c7e87a424b0fb38
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-07 01:22:46 +0100

    removed product.allergeen because it didnt make sense

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,8 +16,8 @@ class ProductController extends Controller
         $products = DB::table('magazijn')
             ->join('product', 'magazijn.Product_id', '=', 'product.id')
             ->join('productperleverancier', 'product.id', '=', 'productperleverancier.ProductId')
-            ->join('leverancier', 'product_per_leverancier.LeverancierId', '=', 'leverancier.id')
-            ->select('product.Barcode', 'product.Naam', 'magazijn.VerpakkingsEenheid', 'magazijn.AantalAanwezig', 'product.Allergeen', 'leverancier.Naam as LeverancierNaam')
+            ->join('leverancier', 'productper_leverancier.LeverancierId', '=', 'leverancier.id')
+            ->select('product.Barcode', 'product.Naam', 'magazijn.VerpakkingsEenheid', 'magazijn.AantalAanwezig', 'leverancier.Naam as LeverancierNaam')
             ->get();
 
         return view('products.index', ['products' => $products]);
