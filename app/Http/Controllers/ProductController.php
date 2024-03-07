509963ac865184b2f4c1ad65b3cdc8ac4b9cd257
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-07 01:14:39 +0100

    Add join query to retrieve product information in ProductController@index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Magazijn;
+use App\Models\ProductPerLeverancier;
+use App\Models\Leverancier;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = DB::table('magazijn')
+            ->join('product', 'magazijn.Product_id', '=', 'product.id')
+            ->join('product_per_leverancier', 'product.id', '=', 'product_per_leverancier.ProductId')
+            ->join('leverancier', 'product_per_leverancier.LeverancierId', '=', 'leverancier.id')
+            ->select('product.Barcode', 'product.Naam', 'magazijn.VerpakkingsEenheid', 'magazijn.AantalAanwezig', 'product.Allergeen', 'leverancier.Naam as LeverancierNaam')
+            ->get();
+
+        return view('products.index', ['products' => $products]);
+    }
 }
