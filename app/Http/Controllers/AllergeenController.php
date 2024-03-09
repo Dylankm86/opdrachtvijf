400de2ea9e1065ad6f3b81274a5b0fa2c388cb8b
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-09 22:50:21 +0100

    i get an stdclass eroor so i am gonna try something else

--- app/Http/Controllers/AllergeenController.php
+++ app/Http/Controllers/AllergeenController.php
@@ -9,20 +9,21 @@ class AllergeenController extends Controller
 {
     public function show($name)
     {
-        $allergeen = DB::table('allergeen')
-            ->where('Naam', $name)
-            ->first();
+        // $allergeen = DB::table('allergeen')
+        //     ->where('Naam', $name)
+        //     ->first();
 
-        if ($allergeen === null) {
-            return redirect()->back()->with('error', 'Allergeen not found');
-        }
+        // if ($allergeen === null) {
+        //     return redirect()->back()->with('error', 'Allergeen not found');
+        // }
 
-        $products = DB::table('productperallergeen')
-            ->join('product', 'productperallergeen.product_id', '=', 'product.id')
-            ->where('allergeen_id', $allergeen->id)
-            ->select('product.Naam', 'productperallergeen.*')
-            ->get();
+        // $products = DB::table('productperallergeen')
+        //     ->join('product', 'productperallergeen.product_id', '=', 'product.id')
+        //     ->where('allergeen_id', $allergeen->id)
+        //     ->select('product.Naam', 'productperallergeen.*')
+        //     ->get();
 
-        return view('allergeen.show', ['allergeen' => $allergeen, 'products' => $products]);
+        // return view('allergeen.show', ['allergeen' => $allergeen, 'products' => $products]);
+
     }
 }
