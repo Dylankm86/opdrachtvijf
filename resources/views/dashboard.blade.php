015c53e9f705b090d30a962247e7c7a15f7b78b5
Dylankm86 <335478@student.mobutrecht.nl>
2024-03-17 10:36:25 +0100

    Update allergeen link text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -92,7 +92,7 @@
             <td>{{ $product->VerpakkingsEenheid }}</td>
             <td>{{ $product->AantalAanwezig }}</td>
             <td><a href="{{ route('leverancier.show', $product->LeverancierNaam) }}">{{ $product->LeverancierNaam }}</a></td>
-            <td><a href="{{ route('allergeen.show', $product->Naam) }}">meer info</a></td>
+            <td><a href="{{ route('allergeen.show', $product->Naam) }}">Allergeen info</a></td>
         </tr>
         @endforeach
     </tbody>
